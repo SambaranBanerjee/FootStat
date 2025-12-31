0547c5c8344b2f83892604a50609e321c3e21b70
SambaranBanerjee <sanbaranb25@gmail.com>
2025-12-31 12:45:39 +0530

    basic changes to navbar and home page.

---
+++ frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav id='nav-1'>
+        <button>View Players</button>
+        <button>Organizations</button>
+        <button>About Us</button>
+    </nav>
+  )
+}
+
+export default Navbar
