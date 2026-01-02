da4613ebea9c20322acc43e9f4649ea2be1f2360
SambaranBanerjee <sanbaranb25@gmail.com>
2026-01-02 19:58:21 +0530

    Added the Navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Navbar.css";
 
-function Navbar() {
+const Navbar = ({ isAuthenticated }) => {
   return (
-    <nav id='nav-1'>
-        <button>View Players</button>
-        <button>Organizations</button>
-        <button>About Us</button>
-    </nav>
-  )
-}
+    <nav className="navbar">
+      <div className="logo">FootStat</div>
 
-export default Navbar
+      <div className="nav-links">
+        {!isAuthenticated ? (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup" className="highlight">Signup</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/players">View Players</Link>
+            <Link to="/organizations">View Organizations</Link>
+            <Link to="/about">About Us</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
